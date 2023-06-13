38ca23a24912e4b695ee016d31d66cea24932e40
Yuliya Filimonova <yuliya_nova@icloud.com>
2023-06-13 22:23:47 +0600

    Initial commit with .gitignore

---
+++ UIRangeSlider/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  UIRangeSlider
+//
+//  Created by Юлия Филимонова on 13.06.2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
---
+++ UIRangeSlider/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIRangeSlider
+//
+//  Created by Юлия Филимонова on 13.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+
+
+}
+
